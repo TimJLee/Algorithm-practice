7784a0be3affa78145885c0e0b5e1bbc567e8ba7
TimJLee <tim0409@hotmail.com>
2019-07-10 21:40:48 +0900

    2193

---
+++ 동적계획/2193.cpp
@@ -0,0 +1,38 @@
+
+#include <cstdio>
+#include <vector>
+int main(){
+  int i,j,N;
+  scanf("%d",&N);
+  std::vector<long long int> T(N+1,0); // long long int = long long ... 거의 100조까지 커버함
+  T[1]=T[2]=1;
+  for(i=3;i<=N;i++){
+    T[i]=T[i-1]+T[i-2];
+  }
+  printf("%lld",T[N]); //와 미친 ... long long 형은 lld 로 출력해야함.
+}
+
+/*
+1
+10
+100 101
+1000 1001 1010
+10000 10001 10010 10100 10101
+
+0
+0 1
+0 1 0
+0 1 0 0 1
+0 1 0 0 1 0 1 0
+0 1 0 0 1 0 1 0 0 1 0 0 1
+
+피보나치
+N	result
+1 1
+2	1
+3	2
+4	3
+5	5
+6	8
+7	13
+*/
