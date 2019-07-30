6e5774eed6043bb284d76784ecbaf07e8fca387b
TimJLee <tim0409@hotmail.com>
2019-07-30 19:48:39 +0900

    11055 solved

--- 동적계획/11055.cpp
+++ 동적계획/11055.cpp
@@ -1,4 +1,28 @@
 #include <cstdio>
+
+int main(){
+  int A[1001],T[1001];
+  int N,i,j,max;
+  scanf("%d",&N);
+  for(i=1;i<=N;i++) scanf("%d",&A[i]);
+
+  T[1]=A[1];
+  max=T[1];
+  for(i=2;i<=N;i++){
+    T[i]=A[i];
+    for(j=1;j<i;j++){
+      if((A[i]>A[j]) && (A[i]+T[j]>T[i])){
+        T[i]=A[i]+T[j];
+      }
+    }
+    if(max<T[i]) max=T[i];
+  }
+  printf("%d\n",max);
+}
+
+
+/*
+#include <cstdio>
 #include <vector>
 int main(){
   int i,j,k,l,result=0,sum,N;
@@ -22,3 +46,4 @@ int main(){
   }
   printf("%d",result);
 }
+*/
