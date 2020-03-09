a0e23e5e0ced959674c823057f68dc65ffa16e7d
TimJLee <tim0409@hotmail.com>
2020-03-09 16:10:04 +0900

    :ok_hand:solve:14499

---
+++ 구현/14499.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+
+using namespace std;
+
+void changeDice(int dice[],int f){
+  int temp[7]={0};
+  for(int i=1;i<=6;i++){
+      temp[i]=dice[i];
+  }
+  if(f == 1){
+      dice[1]=temp[4];
+      dice[3]=temp[1];
+      dice[4]=temp[6];
+      dice[6]=temp[3];
+  }else if(f==2){
+      dice[1]=temp[3];
+      dice[3]=temp[6];
+      dice[4]=temp[1];
+      dice[6]=temp[4];
+  }else if(f==3){
+      dice[1]=temp[5];
+      dice[2]=temp[1];
+      dice[5]=temp[6];
+      dice[6]=temp[2];
+  }else{
+      dice[1]=temp[2];
+      dice[2]=temp[6];
+      dice[5]=temp[1];
+      dice[6]=temp[5];
+  }
+
+}
+
+
+
+int main(){
+  int N,M,x,y,num,i,j,f;
+  int newX,newY;
+  int dx[]={0,0,-1,1};
+  int dy[]={1,-1,0,0};
+  int dice[7]={0};
+  cin>>N>>M>>x>>y>>num;
+  int map[N][M];
+  for(i=0;i<N;i++){
+      for(j=0;j<M;j++){
+          cin>>map[i][j];
+      }
+  }
+ 
+  for(i=0;i<num;i++){
+      cin>>f;
+      newX = x+dx[f-1];
+      newY = y+dy[f-1];
+      if((newX >= N) || (newY >= M) || (newX < 0) || (newY < 0)) continue;
+      x = newX;
+      y = newY;
+
+      changeDice(dice,f);
+      
+      if(map[x][y] == 0){
+          map[x][y] = dice[6];
+      }else{
+          dice[6] = map[x][y];
+          map[x][y]=0;
+      }
+      cout << dice[1] <<endl;
+  }
+
+ 
+
+  
+}
