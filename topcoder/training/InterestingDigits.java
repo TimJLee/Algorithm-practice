19f4b8cb4845742b339cff5ab5275aa3b61fd4f3
TimJLee <tim0409@hotmail.com>
2020-05-14 23:14:08 +0900

    수학적 풀이 추가(모듈러 합동 연산)

--- topcoder/training/InterestingDigits.java
+++ topcoder/training/InterestingDigits.java
@@ -3,15 +3,34 @@ public class InterestingDigits {
 	public static void main(String[] args) {
 		InterestingDigits it = new InterestingDigits();
 		
+		/*
 		int ans10[] = it.digits(10);
 		int ans3[] = it.digits(3);
 		int ans9[] = it.digits(9);
 		int ans26[] = it.digits(26);
 		int ans30[] = it.digits(30);
+		*/
+		int ans10[] = it.digitsMath(10);
+		int ans3[] = it.digitsMath(3);
+		int ans9[] = it.digitsMath(9);
+		int ans26[] = it.digitsMath(26);
+		int ans30[] = it.digitsMath(30);
 
 		for(int a : ans30) System.out.print(a+" ");
 	}
 	
+	public int[] digitsMath(int base) {
+		Vector<Integer> v = new Vector<Integer>();
+		
+		for(int i=2;i<base;i++) {
+			if(((base-1) % i) == 0 ) v.add(i); // 약수라면 추가한다
+		}
+		
+		int ans[] = new int[v.size()];
+		for(int i=0;i<v.size();i++) ans[i]=v.get(i); 
+		
+		return ans;
+	}
 	public int[] digits(int base) {
 		ArrayList <Integer> v = new ArrayList<Integer>();
 		int mul=0,a=0,b=0,c=0,sum=0;
