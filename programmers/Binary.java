6a014a14acaefb8cf7a85ecc834cc2bc7b52dcda
TimJLee <tim0409@hotmail.com>
2020-05-24 19:20:13 +0900

    진수변환 complete

--- programmers/Binary.java
+++ programmers/Binary.java
@@ -1,17 +1,25 @@
 
-using System;
-import java.lang.Math;
-public class test {
+ 
+import java.util.*;
+public class Binary {
+	public static void main(String[] args) {
+		Binary t1 =new Binary();
+		for(int i=1;i<=10;i++)
+			System.out.println(t1.solution(i));
+		System.out.println(t1.solution(1000000000));
+
+	}
     public long solution(long n) {
+    	/*
         long T[]=new long[(int) (n+1)];
         T[1]=1;
         T[2]=3;
-        T[3]=4;
+        T[3]=4; 
         T[4]=9;
         for(int i=5;i<T.length;i++){
             for (int k = 3; i <= n/2; i++) {
                 if (Math.pow(2, k) == i) {
-                    T[i] = Math.pow(3,k);
+                    T[i] = (long) Math.pow(3,k);
                     break;
                 }
             }
@@ -20,6 +28,25 @@ public class test {
             }
         }
        
-        return T[n];
+        return T[(int) n];
+        */
+    	List<Integer> bin = new ArrayList<>();
+    	
+    	while(true) {
+    		bin.add((int)n%2);
+    		if(n==0) break;
+    		n = n/2;
+    	}
+    	
+    	long sum=0;
+    	for(int i=0;i<bin.size();i++)
+    		sum += Math.pow(3, i)*bin.get(i);
+    	
+    	return sum;
     }
 }
+
+/*
+9번째 숫자 -> 1001(2)번째 숫자 -> 1001(3) = 28(10)
+비트마스크. 진수변환
+*/
