cfd1c656b3cf8a1cba547d3372cee3cee53f4a06
TimJLee <tim0409@hotmail.com>
2020-05-27 17:27:14 +0900

    124나라 다른 풀이 이해

--- programmers/나라의숫자.java
+++ programmers/나라의숫자.java
@@ -15,3 +15,17 @@ class Solution {
         return a;
     }
 }
+
+// 다른풀이
+class Solution {
+	  public String solution(int n) {
+	      String[] num = {"4","1","2"};
+	      String answer = "";
+
+	      while(n > 0){
+	          answer = num[n % 3] + answer;
+	          n = (n - 1) / 3; //wow...
+	      }
+	      return answer;
+	  }
+	}
