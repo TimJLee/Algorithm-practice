0595376819cfebafcaa88e04a51da71ebce07f3a
TimJLee <tim0409@hotmail.com>
2020-09-03 16:56:18 +0900

    길이 format 이해

--- programmers/비밀지도.java
+++ programmers/비밀지도.java
@@ -4,7 +4,11 @@ class Solution {
         String[] answer = new String[arr1.length];
         for(int i=0;i<arr1.length;i++){
             String a = Integer.toBinaryString(arr1[i] | arr2[i]);
-            a = String.format("%" + n + "s", a);
+            //a = String.format("%" + n + "s", a);
+	    // %- 를 쓰면 오른쪽을 공백으로 메꿈. 위는 왼쪽을 공백으로 메꾸는 상황. 아래 while 코드로 해도 가능.
+	    while(a.length()!=n){
+                a = " "+a;
+            }
             a = a.replace("0", " ");
             a = a.replace("1", "#");
             answer[i]=a;
